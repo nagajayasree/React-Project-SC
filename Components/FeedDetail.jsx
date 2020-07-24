9bfff7903a90cf075b1a76df197ef5fb15cb52da
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-07-24 23:27:45 +0530

    Update FeedDetail.jsx

--- Components/FeedDetail.jsx
+++ Components/FeedDetail.jsx
@@ -6,12 +6,24 @@ const FeedDetail = ({ match, history }) => {
   // console.log(match.params);
   return (
     <>
-      <button className="backBtn" onClick={() => history.push("/feedlist")}>
-        Back
-      </button>
-      <div>Feed Detail of Article{match.params.id}</div>
-      <h3>{match.params.title}</h3>
-      <p>{match.params.desc}</p>
+      <Container>
+        <button className="backBtn" onClick={() => history.push("/feedlist")}>
+          Back
+        </button>
+        {/* <div>Feed Detail of Article{match.params.id}</div> */}
+        <Row>
+          <Col sm={8} className="feed_detail">
+            <h3 className="feed_title">{match.params.title}</h3>
+            <p className="feed_desc">{match.params.desc}</p>
+          </Col>
+          <Col sm={3} className="vi_list">
+            <p>
+              list of videos list of videos list of videos list of videos list
+              of videos list of videos
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
