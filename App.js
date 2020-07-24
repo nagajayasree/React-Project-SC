2af0cdd973354b7fea26246022a258a7ef66ca19
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-07-24 16:52:51 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar2 from "./Components/NavBar2";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar2 />
+    </div>
+  );
+}
+
+export default App;
