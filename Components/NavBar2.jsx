4f121df3dd56945b5cd66cf0d1b48b8b3961d7a9
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-07-24 16:51:23 +0530

    Update NavBar2.jsx

--- Components/NavBar2.jsx
+++ Components/NavBar2.jsx
@@ -31,13 +31,21 @@ class NavBar2 extends Component {
             </Col>
             <Col sm={9}>
               <Switch>
+                ///displays the feedDetails of selected feedlist
+                <Route
+                  path="/feedDetail/:id/:title/:desc"
+                  component={FeedDetail}
+                />
                 <Route path="/feedlist" component={FeedList} />
                 <Route path="/videolist" component={VideoList} />
+                ///displays this page automatically when page is loaded
+                <Redirect from="/" exact to="/feedlist"></Redirect>
                 <Route path="/feedDetail" component={FeedDetail} />
                 <Redirect from="/feeds" to="/feedlist" />
                 <Redirect from="/videos" to="/videolist" />
-                <Route from="/feed" component={NotFound} />
-                <Route from="/video" component={NotFound} />
+                ///displays NotFound comp if url doesn't match with the above
+                paths
+                <Route to="/not-found" component={NotFound} />
               </Switch>
             </Col>
           </Row>
