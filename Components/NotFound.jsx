58d01347df47509ac3c3605aeacd52d4df70457a
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-07-24 16:53:55 +0530

    Update NotFound.jsx

--- Components/NotFound.jsx
+++ Components/NotFound.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import err from "./errorImg.PNG";
+import { Image, Row, Col } from "react-bootstrap";
 
 const NotFound = () => {
   return (
     <div>
-      <h1>Page Not Found</h1>
+        <h1>
+        <Row>
+          <Col sm={12}>
+            <h4 style={{ textAlign: "center", marginTop: "30px" }}>
+              Page Not Found!
+            </h4>
+            <Image
+              style={{ marginTop: "50px", marginLeft: "220px" }}
+              src={err}
+              alt="image"
+            />
+          </Col>
+        </Row>
+      </h1>
     </div>
   );
 };
