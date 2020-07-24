b1e882fd68c32db9d3580ea88923d3dff7d204df
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-07-24 16:42:42 +0530

    Update FeedDetail.jsx

--- Components/FeedDetail.jsx
+++ Components/FeedDetail.jsx
@@ -1,18 +1,20 @@
 import React, { Component, Fragment } from "react";
-import { Container } from "react-bootstrap";
+import { Container,Row,Col } from "react-bootstrap";
 import "./FeedDetail.css";
 
-class FeedDetail extends Component {
-  state = {};
-  render() {
-    return (
-      <Fragment>
-        <Container>
-          <h3>FeedDetail</h3>
-        </Container>
-      </Fragment>
-    );
-  }
-}
+const FeedDetail = ({ match, history }) => {
+  // console.log(match.params);
+  return (
+    <>
+      <button className="backBtn" onClick={() => history.push("/feedlist")}>
+        Back
+      </button>
+      <div>Feed Detail of Article{match.params.id}</div>
+      <h3>{match.params.title}</h3>
+      <p>{match.params.desc}</p>
+    </>
+  );
+};
+
 
 export default FeedDetail;
