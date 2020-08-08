740e2b00cd8587ac4529e404ac6a7652d4c4fc0c
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-08-08 22:07:07 +0530

    Update VideoDetail.jsx

--- Components/VideoDetail.jsx
+++ Components/VideoDetail.jsx
@@ -9,22 +9,23 @@ const VideoDetail = ({ match, history }) => {
       <Container>
         <Row>
           <Col sm={8} className="video_detail">
-              <iframe
-                className="videoDetail_link"
-                src={match.params.link}
-                //   src={"https://www.youtube.com/embed/kT9sNV9iwRE"}
-                frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; 
-              picture-in-picture"
-                allowfullscreen
-              ></iframe>
+            <iframe
+              width="650"
+              height="415"
+              src={match.params.link}
+                src="https://www.youtube.com/embed/kT9sNV9iwRE"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
             <h3 className="videoDetail_title">{match.params.title}</h3>
             <p className="videoDetail_desc">{match.params.desc}</p>
+            <div className="videoDetail_feedList">
+              <FeedList />
+            </div>
           </Col>
           <Col sm={3} className="vdeo_list">
-            <div>
-              <VideoList />
-            </div>
+            <VideoList />
           </Col>
         </Row>
       </Container>
