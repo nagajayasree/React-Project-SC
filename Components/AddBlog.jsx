1af40ba9eb7bb69bd5d64fc0f8d460aa97be1f6b
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-09-07 18:50:54 +0530

    Update AddBlog.jsx

--- Components/AddBlog.jsx
+++ Components/AddBlog.jsx
@@ -1,58 +1,100 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./AddBlog.css";
+import Input, { Textarea } from "../OtherComponents/Input";
 
 class AddBlog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    info: {
+      blogName: "",
+      mailId: "",
+      url: "",
+      feed: "",
+    },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+    const { info } = this.state;
+    if (info.blogName.trim() === "") {
+      errors.blogName = "Name is Required.";
+    }
+    if (info.mailId.trim() === "") {
+      errors.mailId = "Email-id is Required.";
+    }
+    if (info.url.trim() === "") {
+      errors.url = "URL is Required.";
+    }
+    if (info.feed.trim() === "") {
+      errors.feed = "Feed is Required.";
+    }
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate();
+    // console.log(errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+    // console.log("submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const info = { ...this.state.info };
+    info[input.name] = input.value;
+    this.setState({ info });
+  };
+
   render() {
+    const { info, errors } = this.state;
+
     return (
       <>
         <Container fluid>
-          <form action="" className="AddBlogForm">
-            <h5 className="AddBlogHead">Add Your Blog</h5>
+          <h5 className="AddBlogHead">Add Your Blog</h5>
+          <form className="AddBlogForm" onSubmit={this.handleSubmit}>
             <Row>
               <Col sm={9} className="addBlog">
-                <Row sm={4}>
-                  <label htmlFor="name">
-                    <Col sm={4}>Name</Col>
-                  </label>
-                  <Col sm={4}>:</Col>
-                  <Col sm={4}>
-                    <input type="text" className="addblog_name" />
-                  </Col>
+                <Row sm={4} className="inputField">
+                  <Input
+                    name="blogName"
+                    value={info.blogName}
+                    label="Name"
+                    onChange={this.handleChange}
+                    type="text"
+                    error={errors.blogName}
+                  />
                 </Row>
-                <Row sm={4}>
-                  <label htmlFor="mailId">
-                    <Col sm={4}>Email</Col>
-                  </label>
-                  <Col sm={4}>:</Col>
-                  <Col sm={4}>
-                    <input type="email" className="addblog_email" />
-                  </Col>
+                <Row sm={4} className="inputField">
+                  <Input
+                    name="mailId"
+                    value={info.mailId}
+                    label="Email-Id"
+                    onChange={this.handleChange}
+                    type="email"
+                    error={errors.mailId}
+                  />
                 </Row>
-                <Row sm={4}>
-                  <label htmlFor="url">
-                    <Col sm={4}>URL</Col>
-                  </label>
-                  <Col sm={4}>:</Col>
-                  <Col sm={4}>
-                    <input type="url" className="addblog_url" />
-                  </Col>
+                <Row sm={4} className="inputField">
+                  <Input
+                    name="url"
+                    value={info.url}
+                    label="URL"
+                    onChange={this.handleChange}
+                    type="url"
+                    error={errors.url}
+                  />
                 </Row>
-                <Row sm={4}>
-                  <label htmlFor="feed">
-                    <Col sm={4}>Feed</Col>
-                  </label>
-                  <Col sm={4}>:</Col>
-                  <Col sm={4}>
-                    <textarea
-                      className="addblog_url"
-                      rows="6"
-                      cols="50"
-                      maxLength="100"
+                <Row sm={4} className="inputField">
+                  <Col sm={12}>
+                    <Textarea
+                      name="feed"
+                      value={info.feed}
+                      onChange={this.handleChange}
+                      type="text"
+                      error={errors.feed}
                     />
                   </Col>
                 </Row>
@@ -60,7 +102,7 @@ class AddBlog extends Component {
             </Row>
             <Row>
               <Col sm={4}>
-                <input type="submit" value="Submit" className="addBlg_Subtn" />
+                <button className="btn btn-primary addBlg_Subtn">Submit</button>
               </Col>
             </Row>
           </form>
