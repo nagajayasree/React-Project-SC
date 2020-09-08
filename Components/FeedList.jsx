9f9f41b2dfac3faf14ecc158f9d051bd300787ca
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-09-08 21:46:18 +0530

    Update FeedList.jsx

--- Components/FeedList.jsx
+++ Components/FeedList.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import AddPagination from "../OtherComponents/AddPagination";
 import { getfeedPosts } from "../DataServices/fakeFeedPostService";
 import { paginate } from "../OtherComponents/paginate";
+import SearchBar from "../OtherComponents/SearchBar";
 import _ from "lodash";
 
 class FeedList extends Component {
@@ -16,7 +17,7 @@ class FeedList extends Component {
       currentPage: 1,
       pageSize: 4,
       sortPosts: { path: "title", order: "desc" },
-
+      searchQuery="",
     };
   }
   
@@ -28,29 +29,59 @@ class FeedList extends Component {
     this.setState({ currentPage: page });
   };
 
+ handleSearch = (query) => {
+    this.setState({ searchQuery: query, currentPage: 1 });
+  };
+
+
   render() {
-    const { feedPosts, currentPage, pageSize } = this.state;
-    const sorted = _.orderBy(feedPosts, [sortPosts.path], [sortPosts.order]);
+    const {
+      feedPosts,
+      currentPage,
+      sortPosts,
+      pageSize,
+      searchQuery,
+      feedPosts: allPosts,
+    } = this.state;
+
+    let filtered = allPosts;
+    if (searchQuery)
+      filtered = allPosts.filter((p) =>
+        p.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+
+    const sorted = _.orderBy(filtered, [sortPosts.path], [sortPosts.order]);
+
     const FeedPosts = paginate(sorted, currentPage, pageSize);
+
     return (
       <Fragment>
         <Container fluid>
           <Row>
-            <Col sm={9} className="col-1">
+            <Col sm={9} className="feed_col_1">
+              <Col sm={9}>
+                <SearchBar
+                  value={searchQuery}
+                  onChange={this.handleSearch}
+                  classes="form-comtrol my-3 feed_search"
+                />
+              </Col>
+              <p>Feed List</p>
               {FeedPosts.map((e) => {
                 return (
                   <ul key={e.id} className="feed-item">
-                    <a>
-                      <div>
-                        <Image alt="thumbnail" src={pic} className="img" />
+                    <div>
+                      <Image alt="thumbnail" src={pic} className="feed_img" />
+                    </div>
+                    <Link
+                      to={`/feedDetail/${e.id}/${e.title}/${e.description}`}
+                      className="feed_title"
+                    >
+                      <div className="feed_content">
+                        <h5 className="feed_heading1">{e.title}</h5>
+                        <p className="feed_heading2">{e.description}</p>
                       </div>
-                         <Link to="/feedDetail/${e.id}/${e.title}/${e.description}" className="title">
-                            <div className="content">
-                               <h5 className="heading1">{e.title}</h5>
-                               <p className="heading2">{e.description}</p>
-                        </div>
-                        </Link>
-                    </a>
+                    </Link>
                   </ul>
                 );
               })}
